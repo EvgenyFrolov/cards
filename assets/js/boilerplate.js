d23b5024f9415fd06684579a11f6a0eb3b36f84f
Evgeny Frolov <johnwfrolov@gmail.com>
2016-10-05 01:41:35 +0300

    Updated libs. New modulear structure. ( boilerplate-file has been created in oder to test lips dependency ) removed content from index.html (temporary)

---
+++ assets/js/boilerplate.js
@@ -0,0 +1,18 @@
+define([
+
+	'jquery',
+	'underscore',
+   	'bootstrap',
+    'bootstrapcombobox',
+   	'bootstrapselect',
+    'datatables',
+    'backbone',
+    'backendless',
+ 	'backboneBackendless',
+
+],	function() {
+
+	console.log("Hello Worlds");
+
+	return {};
+});
---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+// Author: John Frolov <johnwfrolov@gmail.com>
+// Filename: main.js
+
+require.config({
+
+	shim: {
+		'jquery': {
+			exports: '$'	
+		},
+
+		'underscore': {
+			exports: '_'
+		},
+
+		'bootstrap': {
+			deps : ['jquery'],
+			exports: 'Bootstrap'
+		
+        },
+
+    	'bootstrapcombobox': {
+    		deps : ['jquery'],
+    		exports: '$.fn.combobox'
+    	},
+
+    	'bootstrapselect': {
+    		deps : ['jquery'],
+    		exports: '$.fn.select'
+    	},
+
+    	'DataTables': {
+    		deps : ['jquery'],
+    	},
+
+    	'backbone': {
+    		deps: ['underscore', 'jquery'],
+            exports: 'backbone',
+    	},
+
+    	'backendless': {
+    		exports: 'Backendless',
+	    },
+
+	    'backboneBackendless': {
+	      	deps: ['backendless', 'backbone'],
+	      	exports : 'BackboneBackendless'
+	     },
+	},
+
+	paths: {
+
+    	jquery: 				'lib/jquery/jquery-3.1.1.min',
+    	bootstrap: 				'lib/bootstrap/bootstrap.min',
+    	bootstrapcombobox: 		'lib/bootstrap/bootstrap-combobox',
+    	bootstrapselect: 		'lib/bootstrap/bootstrap-select.min',
+    	datatables: 			'lib/jquery/jquery.dataTables.min',
+    	underscore:  			'lib/underscore/underscore-1.8.3.min',
+    	backbone: 				'lib/backbone/backbone-min',
+    	backendless:        	'lib/backendless/backendless.min',
+    	backboneBackendless:	'lib/backendless/backendless-backbone-min',
+
+  	}
+
+});
+
+require( [
+  		'boilerplate',
+	],
+ 	function(App){
+		//App.initialize();
+});
