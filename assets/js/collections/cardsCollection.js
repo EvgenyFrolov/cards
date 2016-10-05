0eb1cda47da96885a5fda8d4f847dbf594df74ac
Evgeny Frolov <johnwfrolov@gmail.com>
2016-10-05 23:53:36 +0300

    Colections are modules

--- assets/js/collections/cardsCollection.js
+++ assets/js/collections/cardsCollection.js
@@ -1,29 +1,39 @@
-var cardsApp = cardsApp || {}
+define([
 
-cardsApp.CardsCollection = Backbone.Collection.extend({
-	model: cardsApp.CardModel,
-    initialize: function(models, options){
+    'jquery'
+    'underscore',
+    'backbone',
+    'models/cardModel'
 
-    },
-    // use fetch() - for getting all cards
+], function($, _, Backbone, CardModel)  {
 
-    filterByCategory: function(categoryName){
-        var filtered = this.filter(function (card) {
-            return card.get("category") == categoryName;
-        });
-        // creating new instance of collection:
-        return new cardsApp.CardsCollection(filtered);
-        //return filtered;
-    },
-    // returns list of categories
-    getCategories: function(){
-        var categories = [];
-        this.each(function(model, index) {
-            var category = model.get("category");
-            if($.inArray(category, categories) == -1){
-                categories.push(category);
-            }
-        });
-        return categories;
-    }
+
+    var CardsCollection = Backbone.Collection.extend({
+
+    	model: CardModel,
+
+        initialize: function(models, options){
+
+        },
+        // use fetch() - for getting all cards
+
+        filterByCategory: function(categoryName){
+            var filtered = this.filter( function (card) {
+                return card.get("category") === categoryName;
+            });
+            return new cardsApp.CardsCollection(filtered);
+        },
+
+        getCategories: function(){
+            var categories = [];
+            this.each(function(model, index) {
+                var category = model.get("category");
+                if($.inArray(category, categories) == -1){
+                    categories.push(category);
+                }
+            });
+            return categories;
+        }
+
+    });
 });
--- assets/js/collections/categoriesCollection.js
+++ assets/js/collections/categoriesCollection.js
@@ -1,20 +1,34 @@
-var cardsApp = cardsApp || {}
+define([
 
-cardsApp.CategoriesCollection = Backbone.Collection.extend({
-	model: cardsApp.CategoryModel,
-    initialize: function(models, options){
+    'underscore',
+    'backbone',
+    'models/cardModel'
 
-    },
-    // pass "All" or empty value as 'category' if you want to fetch all categories
-    fetchCategory: function(category, options){
-        // where userId = [current user id]
-        var condition = "userId=" + encodeURIComponent("'" + appModel.get("loggedUser").objectId + "'");
-        // where userId = [current user id] AND categoryName = [category]
-        if (category!= undefined && category!="Все"){
-            condition += " AND categoryName=" + encodeURIComponent("'" + category + "'")
-        }
-        var opt = $.extend({}, options, { "condition" : condition });
-        return this.fetch(opt);
-    },
-    // use fetch() - for getting them all
+], function(_, Backbone, CategoryModel) {
+
+
+    var CategoriesCollection = Backbone.Collection.extend({
+
+    	model: CategoryModel,
+
+        initialize: function(models, options){
+
+        },
+
+        // pass "All" or empty value as 'category' if you want to fetch all categories
+        fetchCategory: function(category, options){
+            // where userId = [current user id]
+            var condition = "userId=" + encodeURIComponent("'" + appModel.get("loggedUser").objectId + "'");
+            // where userId = [current user id] AND categoryName = [category]
+            if (category !== undefined && category !== "Все"){
+                condition += " AND categoryName=" + encodeURIComponent("'" + category + "'");
+            }
+            var opt = $.extend({}, options, { "condition" : condition });
+            return this.fetch(opt);
+        },
+        // use fetch() - for getting them all
+    });
+
+    return CategoriesCollection;
+
 });
