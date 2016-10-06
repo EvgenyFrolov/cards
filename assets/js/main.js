b96b0182e3ea48ad7333aeb834636e560ca164c1
Evgeny Frolov <johnwfrolov@gmail.com>
2016-10-07 00:25:28 +0300

    added forgotten template (categoryTableTemplate). added templates path to config

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,70 +3,71 @@
 
 require.config({
 
-	shim: {
-		'jquery': {
-			exports: '$'	
-		},
-
-		'underscore': {
-			exports: '_'
-		},
-
-		'bootstrap': {
-			deps : ['jquery'],
-			exports: 'Bootstrap'
-		
+    shim: {
+        'jquery': {
+            exports: '$'    
         },
 
-    	'bootstrapcombobox': {
-    		deps : ['jquery'],
-    		exports: '$.fn.combobox'
-    	},
+        'underscore': {
+            exports: '_'
+        },
 
-    	'bootstrapselect': {
-    		deps : ['jquery'],
-    		exports: '$.fn.select'
-    	},
+        'bootstrap': {
+            deps : ['jquery'],
+            exports: 'Bootstrap'
+        
+        },
 
-    	'DataTables': {
-    		deps : ['jquery'],
-    	},
+        'bootstrapcombobox': {
+            deps : ['jquery'],
+            exports: '$.fn.combobox'
+        },
 
-    	'backbone': {
-    		deps: ['underscore', 'jquery'],
+        'bootstrapselect': {
+            deps : ['jquery'],
+            exports: '$.fn.select'
+        },
+
+        'DataTables': {
+            deps : ['jquery'],
+        },
+
+        'backbone': {
+            deps: ['underscore', 'jquery'],
             exports: 'Backbone',
-    	},
+        },
 
-    	'backendless': {
-    		exports: 'Backendless',
-	    },
+        'backendless': {
+            exports: 'Backendless',
+        },
 
-	    'backboneBackendless': {
-	      	deps: ['backendless', 'backbone'],
-	      	exports : 'BackboneBackendless'
-	     },
-	},
+        'backboneBackendless': {
+            deps: ['backendless', 'backbone'],
+            exports : 'BackboneBackendless'
+         },
+    },
 
-	paths: {
+    paths: {
 
-    	jquery: 				'lib/jquery/jquery-3.1.1.min',
-    	bootstrap: 				'lib/bootstrap/bootstrap.min',
-    	bootstrapcombobox: 		'lib/bootstrap/bootstrap-combobox',
-    	bootstrapselect: 		'lib/bootstrap/bootstrap-select.min',
-    	datatables: 			'lib/jquery/jquery.dataTables.min',
-    	underscore:  			'lib/underscore/underscore-1.8.3.min',
-    	backbone: 				'lib/backbone/backbone-min',
-    	backendless:        	'lib/backendless/backendless.min',
-    	backboneBackendless:	'lib/backendless/backendless-backbone-min',
+        jquery:                 'lib/jquery/jquery-3.1.1.min',
+        bootstrap:              'lib/bootstrap/bootstrap.min',
+        bootstrapcombobox:      'lib/bootstrap/bootstrap-combobox',
+        bootstrapselect:        'lib/bootstrap/bootstrap-select.min',
+        datatables:             'lib/jquery/jquery.dataTables.min',
+        underscore:             'lib/underscore/underscore-1.8.3.min',
+        backbone:               'lib/backbone/backbone-min',
+        backendless:            'lib/backendless/backendless.min',
+        backboneBackendless:    'lib/backendless/backendless-backbone-min',
+        templates:              '../templates'
 
-  	}
+    }
 
 });
 
 require( [
-  		'cards',
-	],
+        'cards',
+    ],
 
- 	function(App){
-		App.initialize();
+    function(App){
+        App.initialize();
 });
